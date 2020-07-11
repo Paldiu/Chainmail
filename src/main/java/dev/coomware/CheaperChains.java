eff95b1b7bcfbe65937090720184a85e71b51126
Paul Reilly <pawereus@gmail.com>
2020-07-11 14:27:45 -0500

    Fixed up some issues
    fixes

--- src/main/java/dev/coomware/CheaperChains.java
+++ src/main/java/dev/coomware/CheaperChains.java
@@ -4,7 +4,9 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,18 +18,30 @@ public class CheaperChains implements Listener {
 
     //This event priority should make the condition guaranteed
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void chainCraftEvent(@NotNull PrepareItemCraftEvent e) {
+    public void prepareCraftEvent(@NotNull PrepareItemCraftEvent e) {
         //Check to make sure the return value isn't just a repaired item
         if (e.isRepair()) { return; }
 
         final Recipe recipe = e.getRecipe();
-
         //Check to make sure the recipe actually exists
         if (recipe == null) { return; }
+        final ItemStack result = recipe.getResult();
 
         //If the result of the recipe is chains, set the amount to give to 3
-        if (recipe.getResult().getType() == Material.CHAIN) {
-            recipe.getResult().setAmount(3);
+        if (result.getType() == Material.CHAIN) {
+            result.setAmount(3);
+            e.getInventory().setResult(result);
+        }
+    }
+
+    //This whole method does the same as above, just an extra fail-safe.
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void chainCraftEvent(@NotNull CraftItemEvent e) {
+        Recipe recipe = e.getRecipe();
+        ItemStack result = recipe.getResult();
+        if (result.getType() == Material.CHAIN) {
+            result.setAmount(3);
+            e.getInventory().setResult(result);
         }
     }
 }
